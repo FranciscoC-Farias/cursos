d4c6e9f28b14a4e402deaa5ff71e731ac228f69b
Francisco <fariasf571@gmail.com>
2022-07-14 23:42:42 -0300

    raiz/rastreia.rb: reconstrução da função lista_path para listar somente se o nome do arquivo não existir na pasta raiz

--- rastreia.rb
+++ rastreia.rb
@@ -10,24 +10,34 @@
 #   procuraDiretorio(ARGV[0].to_s,ARGV[1].to_s);
 # end
 
-param  = ARGV[0];
-
-
 def lista_path(str)
   Dir.chdir(Dir.pwd) do
+    local = Dir.entries(Dir.pwd)
+    local.each do |pasta|
+      if(pasta == str)
+        exit;
+      end
+    end
     Dir.glob("./**/#{str}");
   end
 end
 
-paths = lista_path(param)
+param = ARGV[0];
+paths = "";
+
+if(param)
+  paths = lista_path(param)
+else
+  print "Informe o nome do diretório que você deseja excluir: ";
+  param = gets.chomp;
+end
+
+paths = lista_path(param);
 
 unless(paths.empty?)
-
   paths.each do |path|
     puts path
   end
-
 else
   puts "#{param} Não encontrado!"
 end
-
