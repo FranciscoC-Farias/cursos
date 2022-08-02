b1808e97ae45aa98f02e30fe2e74e0e6abe367a2
Francisco <francisco@skylan.com.br>
2022-08-02 12:07:26 -0300

    rails/anotacoes/db__seeds.sh: Inserindo instruções sobre os métodos create e find_or_create_by

---
+++ rails/anotacoes/db__seeds.sh
@@ -0,0 +1,5 @@
+< Model.create!() > # Adiciona os dados na tabela, porém ele não faz a verificação se os dados inseridos já existem na tabela ou não!
+
+< Model.find_or_create_by!() > # Esse também adiciona, porém ele faz a virificação, se o dado não existir, ele o insere, se não, não.
+
+
