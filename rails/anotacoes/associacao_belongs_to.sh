fd935359d0df4537bace3bb08daecce8a81ff6a6
Francisco <francisco@skylan.com.br>
2022-08-08 12:59:03 -0300

    rails/anotacoes/associacao_belongs_to.sh: Melhorando explicação do belongs_to

--- rails/anotacoes/associacao_belongs_to.sh
+++ rails/anotacoes/associacao_belongs_to.sh
@@ -10,3 +10,9 @@
 
 #Depois adicionamos o valor do Primeiro Valor da tabela MiningTyope na associação do coin
   c.mining_type = m
+
+
+
+#OBS: Quando criamos uma associação belongs_to, tornamos a coluna associada obrogatória, e podemos resolver da seguinte forma: 
+  # Adicionando o optional: true
+    belongs_to :mining_type, optional: true # Que não é uma boa prática, dessa forma, não criará a relação, pois o campo ficará vazio
