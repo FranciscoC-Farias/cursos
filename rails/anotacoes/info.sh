5b97c6c966a8d45e94c1f6cc54c0a8414ea4905f
Francisco <francisco@skylan.com.br>
2022-08-08 15:12:58 -0300

    rails/anotacoes/info.sh: Passando as informações sobre helpers para helpers.sh

--- rails/anotacoes/info.sh
+++ rails/anotacoes/info.sh
@@ -22,19 +22,6 @@
 #Veja essa documentação: http://ruby-doc.org/stdlib-2.4.0/libdoc/erb/rdoc/ERB.html
 
 
-#Helpers: São métodos prontos que podem ser usados nas views. E por já estarem prontos facilita a vida do programador, por não precisar escrever tanto código;
-#documentação: https://api.rubyonrails.org/classes/ActionController/Helpers.html   -v 7.0.3 
-#OBS: Devemos usar o mínimo possível de código HTMl, sempre que pudermos, devemos usar códigos ruby:
-#Por exemplo para informarmos que utilizaremos links, ao invés de usarmos a tag "a" podemos usar o método 'link_to "Descrição", <rota_path ou url>' 
-<%= link_to "Cadastrar moedas" , coins_path %>
-<%= image_tag coin.url_image, size: 25x25 %>
-#documentação: https:://api.rubyonrails.org/classes/ActiveonView/Helpers/AssetTagHelper.html#method-i-image_tag
-
-
-
-
-
-
 
 #### Para levantar a aplicação em ambiente diferente, de produção ####
   RAILS_ENV=production rails s 
